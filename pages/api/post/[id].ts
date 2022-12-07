273699bc5b163c33986991975f14d296663de42b
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2022-12-07 13:00:38 +0100

    chore: change permissions in dashboard

--- pages/api/post/[id].ts
+++ pages/api/post/[id].ts
@@ -14,6 +14,7 @@ const handler = async (req:NextApiRequest, res:NextApiResponse) => {
       const { post, error } = await updatePost(id, data)
       if (error) throw error
       await res.revalidate(`/posts/${id}`)
+      await res.revalidate(`/posts`)
       return res.status(200).json({ post })
     } catch (error) {
       return res.status(500).json({ error })
